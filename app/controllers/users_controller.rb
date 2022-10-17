52b7d917f59c5aa8c72c8a752cb36ad64851a985
Pete Fowler <fowler.pete@gmail.com>
2022-10-17 15:16:36 -0600

    Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    user = User.find_by(username: params[:username])
-    if session.include? :user_id
+    user = User.find_by(id: session[:user_id])
+    if user
       render json: user
     else
       render json: { error: 'Not authorized' }, status: :unauthorized
