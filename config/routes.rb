c996675907d459b66e561432cef0d630a38ec04d
Pete Fowler <fowler.pete@gmail.com>
2022-10-17 14:39:32 -0600

    Add login/out

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
 
   get '/me', to: 'users#show' 
+
+  post '/login', to: 'sessions#login'
+
+  delete '/logout', to: 'sessions#logout' 
 end
